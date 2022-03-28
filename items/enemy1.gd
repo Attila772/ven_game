5fc1471a44284093f82bfa7bb11ee079b026783e
Attila772 <71554727+Attila772@users.noreply.github.com>
2022-03-28 20:32:06 +0200

    nav

--- items/enemy1.gd
+++ items/enemy1.gd
@@ -6,11 +6,15 @@ var current_col = 0
 var player_pos
 var current_cooldown = -1
 var shot_cooldown = 2
+var move_vector = Vector2()
 
 func _ready():
 
 	pass
-
+func _physics_process(delta):
+	if move_vector.length()>0:
+		move_and_slide(move_vector.normalized()*50)
+		print(rad2deg(move_vector.angle()))
 
 
 	
@@ -26,11 +30,14 @@ func _process(delta):
 	if ded and not $AnimationPlayer.is_playing():
 			get_parent().remove_child(self)
 	else:
-		player_pos=Vector2(get_parent().get_node('Player').get_global_position()-self.get_global_position())
+		player_pos=Vector2(get_parent().get_parent().get_parent().get_node('Player').get_global_position()-self.get_global_position())
 		get_node("RayCast2D").cast_to=player_pos
 		if $RayCast2D.is_colliding() && $RayCast2D.get_collider().name == "Player":
+			var from = get_global_transform().origin
+			var to =  get_parent().get_parent().get_parent().get_node('Player').get_global_transform().origin
+			var line = get_parent().get_parent().get_simple_path(from,to)
+			move_vector = get_global_transform().origin.direction_to(line[1])
 			shoot()
-			print (get_node("RayCast2D").get_collider().name)
 
 
 func got_shot(i = 25):
@@ -46,6 +53,6 @@ func shoot():
 	if current_cooldown < 0:
 		current_cooldown = shot_cooldown
 		var shot = load("res://items/shot_enemy.tscn").instance()
-		shot.vector = transform.get_origin().direction_to((get_parent().get_node('Player').get_global_position()))
+		shot.vector = transform.get_origin().direction_to((get_parent().get_parent().get_parent().get_node('Player').get_global_position()))
 		shot.set_global_transform(self.get_global_transform())
 		get_parent().add_child(shot)
