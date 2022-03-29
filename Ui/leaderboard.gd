e15f73413fadf18884e513951b5b233a96735a38
Attila772 <71554727+Attila772@users.noreply.github.com>
2022-03-29 10:04:09 +0200

    level 2

--- Ui/leaderboard.gd
+++ Ui/leaderboard.gd
@@ -4,16 +4,29 @@ var loaded = false
 func _ready():
 	Http.leaderboard()
 	pass
-
+	
+	
+class custom_sort:
+	static func sort_ascending(a, b):
+		if int(a.split('_')[1]) < int(b.split('_')[1]):
+			return true
+		return false
+	
 func _process(delta):
 	if Http.success and not loaded:
 		loaded = true
 		var data = Http.leaderboard
 		var dynamic_font = DynamicFont.new()
 		dynamic_font.font_data = load("res://Ui/font.tres")
+		var array = []
 		for key in data.result:
-			var str_ = key+" : "+ str(data.result[key])
-			$ScrollContainer/VBoxContainer/Label.text+= str_+"s"+"\n"
+			array.append(key+"_"+str(data.result[key]))
+		array.sort_custom(custom_sort,"sort_ascending")
+		var counter = 1
+		for item in array:
+			var lbl_str = str(counter) + ". "+item.split('_')[0]+" : "+item.split('_')[1]+"s"	
+			$ScrollContainer/VBoxContainer/Label.text+= lbl_str+"\n"
+			counter+=1
 		Http.success = false
 
 
